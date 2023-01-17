fded4b78b45b5029517f3509a1f11f420a8f430f
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-01-17 18:58:01 -0400

    Added functionality to buttons
    Added js script.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const hide = document.querySelector("#singUp");
+const alreadyHaveAccount = document.querySelector("#alreadyHaveAccount");
+
 hide.addEventListener('click', (event) => {
-  console.log("click!");
-  var element = document.getElementById("theBox");
-  element.classList.toggle("hiddenClass");
+  document.getElementById("loginBox").classList.add("hiddenClass");
+  document.getElementById("singUpBox").classList.remove("hiddenClass");
 });
 
+alreadyHaveAccount.addEventListener('click', (event) => {
+  document.getElementById("loginBox").classList.remove("hiddenClass");
+  document.getElementById("singUpBox").classList.add("hiddenClass");
+});
